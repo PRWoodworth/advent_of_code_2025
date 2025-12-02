ab5f513ac6d97f3445e1cc50475997121e4c5c95
Patrick <p77wood@gmail.com>
2025-12-02 16:42:01 -0600

    initial commit

---
+++ day_1/Day1.java
@@ -0,0 +1,14 @@
+public class Day1 {
+    public static void main(String[] args) {
+        Day1 day1 = new Day1();
+        day1.part_1();
+        day1.part_2();
+    }
+
+    private void part_1(){
+        
+    }
+    private void part_2(){
+
+    }
+}
