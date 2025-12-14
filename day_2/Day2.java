4943e47691a94cad51b662a3a29b9e6b7af4f3ea
Patrick <p77wood@gmail.com>
2025-12-14 15:13:06 -0600

    day 2 part 1

--- day_2/Day2.java
+++ day_2/Day2.java
@@ -3,6 +3,7 @@ package day_2;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Day2 {
@@ -15,17 +16,36 @@ public class Day2 {
     private void part_1() {
         try (BufferedReader br = new BufferedReader(new FileReader("day_2/day2_input.txt"))) {
             String line;
-            int invalid_sum = 0;
+            String filePath = "day_2/output.txt";
+            long invalid_sum = 0;
+            FileWriter writer = new FileWriter(filePath);
             while ((line = br.readLine()) != null) {
                 String [] parts = line.split(",");
                 for(String part : parts){
-                    long lowerBound = Long.parseLong(part.split("-")[0]);
-                    long upperBound = Long.parseLong(part.split("-")[1]);
+                    writer.write("Current range: " + part + "\n");
+                    String split_part[] = part.split("-");
+                    long lowerBound = Long.parseLong(split_part[0]);
+                    long upperBound = Long.parseLong(split_part[1]);
                     for(long i = lowerBound; i <= upperBound; i++){
-
+                        //invalid ID = if first half of number = second half of number (i.e. 11, 1212, 123123, etc.)
+                        String current_str = Long.toString(i);
+                        int str_len = current_str.length();
+                        writer.write("Checking ID: " + current_str + "\n");
+                        if(str_len % 2 == 0){
+                            String first_half = current_str.substring(0, str_len / 2);
+                            String second_half = current_str.substring(str_len / 2);
+                            if(first_half.equals(second_half)){
+                                writer.write("Invalid ID found: " + i + "\n");
+                                invalid_sum += i;
+                            } 
+                        } else {
+                            continue;
+                        }
                     }
                 }
             }
+            writer.close();
+            System.out.println("Part 1: " + invalid_sum);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -37,7 +57,15 @@ public class Day2 {
         try (BufferedReader br = new BufferedReader(new FileReader("day_1/day1_input.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
-                
+                String [] parts = line.split(",");
+                for(String part : parts){
+                    String split_part[] = part.split("-");
+                    long lowerBound = Long.parseLong(split_part[0]);
+                    long upperBound = Long.parseLong(split_part[1]);
+                    for(long i = lowerBound; i <= upperBound; i++){
+                        
+                    }
+                }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
